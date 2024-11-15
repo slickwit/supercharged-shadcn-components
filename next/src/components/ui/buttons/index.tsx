fa5780740e19c1964a995d562b54a8a328f5ea01
kennethryandy <dykennethryan@gmail.com>
2024-11-16 06:25:29 +0800

    🍀added icon button

--- next/src/components/ui/buttons/index.tsx
+++ next/src/components/ui/buttons/index.tsx
@@ -5,3 +5,4 @@
 export { Button, type ButtonProps } from "./button";
 export { FloatingLabelButon, type FloatingLabelButonProps } from "./floating-label-button";
 export { LinkButton, type LinkButtonProps } from "./link-button";
+export { IconButton, iconButtonVariants, type IconButtonProps } from "./icon-button";
