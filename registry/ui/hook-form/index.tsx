fa7f1668ed51b5c1482047347e3a36044cbc489b
kennethryandy <dykennethryan@gmail.com>
2025-03-01 23:55:54 +0800

    fix: update rfh

--- registry/ui/hook-form/index.tsx
+++ registry/ui/hook-form/index.tsx
@@ -3,22 +3,22 @@
 // ----------------------------------------------------------------------
 
 export { default } from "./form-provider";
-export { default as RHFInput } from "./rhf-input";
-export { default as RHFTextarea } from "./rhf-textarea";
-export { default as RHFFloatingInput } from "./rhf-floating-input";
-export { default as RHFAutocomplete } from "./rhf-autocomplete";
-export { default as RHFSelect } from "./rhf-select";
-export { default as RHFMultiSelect } from "./rhf-multi-select";
-export { default as RHFCheckbox } from "./rhf-checkbox";
-export { default as RHFRadioGroup } from "./rhf-radio-group";
+// export { default as RHFInput } from "./rhf-input";
+// export { default as RHFTextarea } from "./rhf-textarea";
+// export { default as RHFFloatingInput } from "./rhf-floating-input";
+// export { default as RHFAutocomplete } from "./rhf-autocomplete";
+// export { default as RHFSelect } from "./rhf-select";
+// export { default as RHFMultiSelect } from "./rhf-multi-select";
+// export { default as RHFCheckbox } from "./rhf-checkbox";
+// export { default as RHFRadioGroup } from "./rhf-radio-group";
 
-//
-export { default as RHFCombobox } from "./rhf-combobox";
+// //
+// export { default as RHFCombobox } from "./rhf-combobox";
 
-//
-export { default as RHFDatePicker } from "./rhf-date-picker";
-export { default as RHFTimePicker } from "./rhf-time-picker";
+// //
+// export { default as RHFDatePicker } from "./rhf-date-picker";
+// export { default as RHFTimePicker } from "./rhf-time-picker";
 
-//
-export { RHFUpload } from "./rhf-upload";
-export { RHFUploadMultiple } from "./rhf-upload-multiple";
+// //
+// export { RHFUpload } from "./rhf-upload";
+// export { RHFUploadMultiple } from "./rhf-upload-multiple";
